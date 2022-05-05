4f14610923665b01bd5fa980ea342d441a4d42af
Paweł Müller <pawelmuller42@gmail.com>
2022-05-06 01:25:54 +0200

    Rebuilt SourceReader and derivatives from the ground

--- src/utils/source_reader/source_reader.py
+++ src/utils/source_reader/source_reader.py
@@ -1,5 +1,5 @@
 from copy import copy
-from io import TextIOWrapper
+from io import TextIOWrapper, BytesIO
 
 from lexer.dictionaries import EOF
 from utils.exceptions.exceptions import MisnomerExecutiveNotFoundError, MisnomerEncodingError
@@ -9,8 +9,15 @@ from utils.position import Position
 class SourceReader:
     _END_OF_LINE = "\n"
 
-    def __init__(self):
+    def __init__(self, source):
+        self._source = source
         self._position = Position()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._source.close()
 
     def update_position(self, character: str):
         if character == SourceReader._END_OF_LINE:
@@ -21,44 +28,35 @@ class SourceReader:
 
         self._position.character += 1
 
+    def get_next_character(self) -> str:
+        character = self._source.read(1)
+        if not character:
+            character = EOF
+        self.update_position(character)
+        return character
+
     def get_position(self):
         return copy(self._position)
 
 
 class FileSourceReader(SourceReader):
     def __init__(self, source_path: str):
-        super().__init__()
         self._source_path: str = source_path
-        self._source: TextIOWrapper
-        self._load_source()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._source.close()
+        source = self._load_source()
+        super().__init__(source)
 
     def _load_source(self):
         try:
-            self._source = open(self._source_path, "r", encoding="utf-8")
+            source = open(self._source_path, "r", encoding="utf-8")
+            return source
         except FileNotFoundError:
             raise MisnomerExecutiveNotFoundError(self._source_path)
         except ValueError or UnicodeDecodeError as error:
             raise MisnomerEncodingError(error)
 
-    def get_next_character(self) -> str:
-        if character := self._source.read(1):
-            self.update_position(character)
-            return character
-        return EOF
-
 
 class StringSourceReader(SourceReader):
     def __init__(self, code):
-        super().__init__()
-        self._code = code
-
-    def get_next_character(self) -> str:
-        character = self._code[self._position.character] if self._position.character < len(self._code) else EOF
-        self.update_position(character)
-        return character
+        input_bytes = BytesIO(code.encode('utf-8'))
+        source = TextIOWrapper(input_bytes, encoding='utf-8')
+        super().__init__(source)
