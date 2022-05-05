47835a13fd7811d5a4789c886042b6090df0cbf6
Paweł Müller <pawelmuller42@gmail.com>
2022-05-06 01:44:34 +0200

    Updated SourceReader with docstrings.

--- src/utils/source_reader/source_reader.py
+++ src/utils/source_reader/source_reader.py
@@ -1,5 +1,6 @@
 from copy import copy
 from io import TextIOWrapper, BytesIO
+from typing import IO
 
 from lexer.dictionaries import EOF
 from utils.exceptions.exceptions import MisnomerExecutiveNotFoundError, MisnomerEncodingError
@@ -20,6 +21,11 @@ class SourceReader:
         self._source.close()
 
     def update_position(self, character: str):
+        """
+        Updates position depending on character kind.
+
+        :param character: Determines what component of position should be updated
+        """
         if character == SourceReader._END_OF_LINE:
             self._position.line += 1
             self._position.column = 0
@@ -28,14 +34,35 @@ class SourceReader:
 
         self._position.character += 1
 
+    def get_first_character(self) -> str:
+        """
+        Obtains one character from source without updating position (since default is correct).
+
+        :return: string
+        """
+        character = self._source.read(1)
+        if not character:
+            character = EOF
+        return character
+
     def get_next_character(self) -> str:
+        """
+        Obtains one character from source.
+
+        :return: string
+        """
         character = self._source.read(1)
         if not character:
             character = EOF
         self.update_position(character)
         return character
 
-    def get_position(self):
+    def get_position(self) -> Position:
+        """
+        Returns a copy of position reader is currently at.
+
+        :return: Copy of reader position
+        """
         return copy(self._position)
 
 
@@ -45,7 +72,12 @@ class FileSourceReader(SourceReader):
         source = self._load_source()
         super().__init__(source)
 
-    def _load_source(self):
+    def _load_source(self) -> IO:
+        """
+        Loads contents of the file at given source_path.
+
+        :return: IO containing contents of the file at given source_path
+        """
         try:
             source = open(self._source_path, "r", encoding="utf-8")
             return source
