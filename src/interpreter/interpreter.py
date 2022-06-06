d6f9002449dff0453a7ed29ecda5abec705f49dd
Paweł Müller <pawelmuller42@gmail.com>
2022-06-06 18:41:13 +0200

    Added methods for Context class

--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -9,6 +9,23 @@ class Context:
         self.functions = builtin_functions.copy()
         self.variables = {}
 
+    def add_function(self, function_name, function):
+        self.functions[function_name] = function
+
+    def get_function(self, function_name):
+        return self.functions.get(function_name)
+
+    def add_variable(self, variable_name, variable_value):
+        self.variables[variable_name] = variable_value
+
+    def get_variable(self, name):
+        return self.variables.get(name)
+
+    def get_context_copy(self):
+        new_context = Context()
+        new_context.functions = self.functions
+        return new_context
+
 
 class Interpreter:
     def __init__(self, program):
