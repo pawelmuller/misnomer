58fc8e015ceb7e8868876f27b5af5477b09a9cec
Paweł Müller <pawelmuller42@gmail.com>
2022-06-09 16:42:32 +0200

    Added two more builtin functions

--- src/interpreter/builtin_functions.py
+++ src/interpreter/builtin_functions.py
@@ -28,6 +28,8 @@ def convert_to_string(x):
 builtin_functions = {
     "print": print,
     "input": input,
+    "find_max": max,
+    "find_min": min,
     "to_int": convert_to_int,
     "to_float": convert_to_float,
     "to_string": convert_to_string,
